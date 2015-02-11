827808be5422be7e36a73208bd4a9d1eef15e545
Omrika Folkes <omrikafolkes@gmail.com>
2015-02-10 19:19:01 -0500

    clock.js

---
+++ clock.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+		function DisplayTime() {
+			var CurrentTime = new Date(); //gets month, date, year, hours, mis, secs, 
+			var hours = CurrentTime.getHours(); //gets hours from currentTime variable
+			var minutes = CurrentTime.getMinutes(); //gets mins from currentTime variable
+			var seconds = CurrentTime.getSeconds(); //gets secs from currentTime variable
+		}
+});
